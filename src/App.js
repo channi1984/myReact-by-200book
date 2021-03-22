2a76ee421c0d15f80f95ec29a2b20fa466cf2b09
channi84 <channi.kr@gmail.com>
2021-03-22 18:02:02 +0900

    Revert "'a'"
    This reverts commit cd75ed9bd0aab940620543e078385be2efa20da9.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css'
-import ForEach from './ForEach'
+import ArrowFunc from './ArrowFunc'
 
 function App() {
   return (
     <div>
-      <ForEach />
+      <ArrowFunc />
     </div>
   )
 }
